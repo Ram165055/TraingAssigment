c7d8c87e0a30aaae08b87027f3d06d42cb628097
Ram Kumar <ramkr165055@gmail.com>
2021-08-10 20:02:30 +0530

    all question done of logic building

--- logic_building_exercise_v2/QuizGame.java
+++ logic_building_exercise_v2/QuizGame.java
@@ -1,4 +1,8 @@
 package com.assigment.one;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
 class Question{
 	String question;
 	String option1;
@@ -21,8 +25,25 @@ class Question{
 
 public class QuizGame {
 	 public static void main(String[] args) {
-		 Question question=new Question("this is","a","b","c","d","a");
-		
+		 Question question=new Question("2+2","a)4","b)3","c)2","d)0","a");
+		 Question question2=new Question("2*2 ","a)3","b)4","c)5","d)1","b");
+		 HashMap<Integer, Question> m=new  HashMap<>();
+		 m.put(1, question);
+		 m.put(2, question2);
+		 Scanner scanner=new Scanner(System.in);
+		 System.out.println("enter question number from 1 to 2");
+		 int id=scanner.nextInt();
+		 
+		 System.out.println(m.get(id).question);
+		 System.out.println(m.get(id).option1+"    "+m.get(id).option2+"   "+m.get(id).option3+"  "+m.get(id).option4);
+		 System.out.println("enter correct option like a or b or c or d");
+		 String ans=scanner.next();
+		 if(ans.equals(m.get(id).correctAnswer)) {
+			 System.out.println("correct answer");
+		 }
+		 else {
+			 System.out.println("wrong answer\n correct answer is"+m.get(id).correctAnswer);
+		 }
 	}
 
 }
